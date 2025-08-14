4078eb6c425d43253c5d2de8d491bb211937bbcd
Lucas Gómez (KB) <79912231+Pigamer37@users.noreply.github.com>
2025-08-14 17:19:33 +0200

    Add sounds to window controls & view transitions

--- scripts/window.js
+++ scripts/window.js
@@ -1,78 +1,85 @@
 function toggleWinFullScreen(clickedBtn){
-  let window = clickedBtn.parentNode.parentNode.parentNode;
-  if (!document.fullscreenElement){//make FS
-    window.requestFullscreen({navigationUI: "show"}).catch((err) => {
-      alert(`Error attempting to enable fullscreen mode: ${err.message} (${err.name})`, );
-    });
-    return;
-  }else{document.exitFullscreen();}
+  let window=clickedBtn.parentNode.parentNode.parentNode
+  if(!document.fullscreenElement){//make FS
+    fsSnd.play()
+    window.requestFullscreen({ navigationUI: "show" }).catch((err)=>{
+      alert(`Error attempting to enable fullscreen mode: ${err.message} (${err.name})`)
+    })
+    return
+  }else{extfsSnd.play();document.exitFullscreen()}
 }
 function winHandleFullScreen(event){
-  if(document.fullscreenElement){//goint FS
+  if(document.fullscreenElement){//goin FS
     let contents=document.fullscreenElement.getElementsByClassName("content");
     for(const cont of contents){
-      if(cont.style.display==="none"){cont.style.display="block";}
+      if(cont.style.display==="none"){cont.style.display="block"}
     }
     let btnGroups=document.fullscreenElement.getElementsByClassName("title-bar")[0].getElementsByClassName("buttons");
     for(const btnGroup of btnGroups){
       for(const btn of btnGroup.children){
-        if(btn.textContent==="⛶"){btn.textContent="❐";break;} 
+        if(btn.textContent==="⛶"){btn.textContent="❐";break}
       }
     }
-    return;
+    return
   }else{//exiting FS
     let btnGroups=this.getElementsByClassName("title-bar")[0].getElementsByClassName("buttons");
     for(const btnGroup of btnGroups){
       for(const btn of btnGroup.children){
-        if(btn.textContent==="❐"){btn.textContent="⛶";break;} 
+        if(btn.textContent==="❐"){btn.textContent="⛶";break}
       }
     }
   }
 }
 
-for (const a of document.querySelectorAll('button')) {
+let closeSnd=new Audio("/scripts/Back.mp3"),fsSnd=new Audio("/scripts/Click_Channel.mp3"),extfsSnd=new Audio("/scripts/Return_to_Menu.mp3"),transSnd=new Audio("/scripts/Scroll.mp3")
+window.addEventListener("pageswap",(e)=>{
+  if(e.viewTransition)
+    e.viewTransition.ready.then(transSnd.play())
+})
+
+for(const a of document.querySelectorAll('button')){
   switch(a.textContent){
     case "_"://minimize
-      let window=a.parentNode.parentNode.parentNode;
-      window.addEventListener("fullscreenchange", winHandleFullScreen);
-      a.style.cursor="url(https://www.cursor.cc/cursor/533/209/cursor.png) 11 2, pointer";
-      a.addEventListener("click", function (e){
+      let window=a.parentNode.parentNode.parentNode
+      window.addEventListener("fullscreenchange", winHandleFullScreen)
+      a.style.cursor="url(https://www.cursor.cc/cursor/533/209/cursor.png) 11 2, pointer"
+      a.addEventListener("click",function (e){
         if(window===document.fullscreenElement){
-          for (const btn of this.parentNode.children) {
-            if(btn.textContent==="❐"){toggleWinFullScreen(btn);break;}
+          for(const btn of this.parentNode.children){
+            if(btn.textContent==="❐"){toggleWinFullScreen(btn);break}
           }
         }
-        for (const ch of window.children) {
+        for(const ch of window.children){
           if(ch.classList.contains("content")){
-            if(ch.style.display!=="none"){
-              ch.style.display="none";
-            }else{
-              ch.style.display="block";
-            }
+            if(ch.style.display!=="none")
+              ch.style.display="none"
+            else
+              ch.style.display="block"
           }
         }
-      });
-      a.tabIndex=0;
-      a.setAttribute("aria-label", "Minimize section");
-    break;
+      })
+      a.tabIndex=0
+      a.setAttribute("aria-label", "Minimize section")
+    break
     case "⛶"||"❐"://fullscreen
-      if (document.fullscreenEnabled){
-        a.style.cursor="url(https://www.cursor.cc/cursor/533/209/cursor.png) 11 2, pointer";
-        a.addEventListener("click", function (e){
-          toggleWinFullScreen(this);
-        });
-        a.tabIndex=0;
-        a.setAttribute("aria-label", "Fullscreen section");
+      if(document.fullscreenEnabled){
+        a.style.cursor="url(https://www.cursor.cc/cursor/533/209/cursor.png) 11 2, pointer"
+        a.addEventListener("click",function (e){
+          toggleWinFullScreen(this)
+        })
+        a.tabIndex=0
+        a.setAttribute("aria-label", "Fullscreen section")
       }
-    break;
+    break
     case "✕"://close (display:none?)
-      a.style.cursor="url(https://www.cursor.cc/cursor/533/209/cursor.png) 11 2, pointer";
-      a.addEventListener("click", function (e){
-        let window=this.parentNode.parentNode.parentNode;
-        window.parentNode.removeChild(window);
-      });
-      a.tabIndex=0;
-      a.setAttribute("aria-label", "Close or delete section");
-    break;
+      a.style.cursor = "url(https://www.cursor.cc/cursor/533/209/cursor.png) 11 2, pointer";
+      a.addEventListener("click", function (e) {
+        closeSnd.play()
+        let window=this.parentNode.parentNode.parentNode
+        window.parentNode.removeChild(window)
+      })
+      a.tabIndex=0
+      a.setAttribute("aria-label", "Close or delete section")
+    break
   }
 }
